8258aadaad29126d814df8b92aff3a6f03168b6a
LOVINGFINE <601214640@qq.com>
2020-11-03 22:36:41 +0800

    login page style

---
+++ src/components/Pop-ups/index.tsx
@@ -0,0 +1,30 @@
+import React,{useEffect, useState} from 'react'
+interface Iprops {
+    title:string
+    show:boolean
+    setShow:(e:boolean)=>void
+    time?:number
+}
+import style from './index.module.less'
+import {
+    View,
+    
+    } from '@tarojs/components'
+export default (
+    {title,time,show,setShow}:Iprops
+)=>{
+    useEffect(()=>{
+        setTimeout(()=>{
+            setShow(false)
+        },time*1000 || 3000)
+    },[show])
+    return <>
+       {
+           show?(
+            <View className={style.box}>
+                <View className={style.title_box}>{title}</View>
+            </View>
+           ):('')
+       }
+    </>
+}
--- src/pages/user-login/index.tsx
+++ src/pages/user-login/index.tsx
@@ -1,16 +1,45 @@
 import React, { Component,useState,useEffect } from 'react'
 import {
 View,
-Button
+Button,
+Input
 } from '@tarojs/components'
+import CodeIpt from './codeIpt'
 import style from './index.module.less'
+import PopUp from '@/components/Pop-ups/index'
 export default ()=>{
+      const [login_step,setLoginStep] = useState<number>(1)
+      const [code,setCode] = useState<string>('')
+      const [pop_show,setPopShow] = useState<boolean>(false)
    const getUserPhone = (e:any)=>{
         console.log(e);
         // 获取手机号回调
-        
+       
+   }
+   const onSuccess = (e)=>{
+      setCode(e)
+         
+   }
+   const handleSend = ()=>{
+      // 失败弹窗
+      // setPopShow(true) 
+
    }
    return (<View className={style.box} >
-         <Button type="primary" openType="getPhoneNumber" onGetPhoneNumber={(e)=>getUserPhone(e)}>授权登录</Button>
+         <PopUp show={pop_show} setShow={setPopShow} title={'邀请码不存在,请重新输入'} />
+         {login_step===0?(<Button type="primary" openType="getPhoneNumber" onGetPhoneNumber={(e)=>getUserPhone(e)}>授权登录</Button>):('')}
+         {
+               login_step===1?(
+                  <>
+                  <View className={style.code_title}>输入网点邀请码</View>
+                  <View className={style.code_ipt_box}>
+                        <CodeIpt onSuccess={(e)=>onSuccess(e)} />
+                  </View>
+                  </>
+               ):('')
+         }
+         <View className={style.bottom_btn_box}>
+         <Button className={style.bottom_btn} onClick={handleSend}>确认</Button>
+         </View>
    </View>)
   }
